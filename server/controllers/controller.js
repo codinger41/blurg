e3733416a9122a2ef8451046f8e3bbeedb3f0847
leksyib <leksyib14@gmail.com>
2018-08-03 11:30:00 +0100

    add comments

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -6,10 +6,12 @@ exports.scrapeData = (req, res) => {
   const { url } = req.body
   if(url.includes('imdb.com')){
     let json = {}
+    // call imdb endpoint
     request(url, async (error, response, html) => {
       if(!error || (response.statusCode !== 404 || '404')){
         let $ = cheerio.load(html)
         let title, release
+        // find css class with title_wrapper and get data from it
         $('.title_wrapper').filter(function(){
           let data = $(this)
           title = data.children().first().text().trim()
@@ -18,9 +20,22 @@ exports.scrapeData = (req, res) => {
           json.release = release
         })
         json.summary = $('.summary_text').text().trim()
-        const newMovie = await Movie.create(json)
-        console.log(newMovie)
-        res.json(json)
+        if(!json.title){
+          res.json({
+            success: false,
+            message: 'Url is not valid'
+          })
+        }
+        // check if movie exists
+        const movie = await Movie.find({ title: json.title })
+        if(movie){
+          res.json(movie)
+        }else{
+          // add new movie if it doesn't exist
+          const newMovie = await Movie.create(json)
+          console.log(newMovie)
+          res.json(json)
+        }
       }else{
         res.json({
           success: false,
