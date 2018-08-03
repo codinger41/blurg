3e83c4842ffb6d8d416f83a56a4b00103bafc6bc
leksyib <leksyib14@gmail.com>
2018-08-03 13:43:21 +0100

    change client build dir

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
